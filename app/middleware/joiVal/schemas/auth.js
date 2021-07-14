d62fe0d468bdcce293ffb5ab3cb8fd420eef8a59
heihotech <heiho.tech@gmail.com>
2021-07-14 11:56:08 +0700

    fix joi roles signUpPOST.roles

--- app/middleware/joiVal/schemas/auth.js
+++ app/middleware/joiVal/schemas/auth.js
@@ -4,7 +4,7 @@ const signUpPOST = Joi.object().keys({
   username: Joi.string().required(),
   email: Joi.string().required(),
   password: Joi.string().required(),
-  roles: Joi.string().required(),
+  roles: Joi.array().required(),
 });
 const signInPOST = Joi.object().keys({
   username: Joi.string().required(),
