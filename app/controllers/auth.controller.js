823773d1100e3af208e3b531eb7164db1b163081
heihotech <heiho.tech@gmail.com>
2021-07-14 12:56:43 +0700

    add JWT_EXPIRE

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -64,7 +64,7 @@ exports.signin = (req, res) => {
       }
 
       var token = jwt.sign({ id: user.id }, config.secret, {
-        expiresIn: 86400, // 24 hours
+        expiresIn: process.env.JWT_EXPIRE,
       });
 
       var authorities = [];
